80087f1ae92681dd3ed8c096269888c22c7c20dd
chapuzzo <luismax@gmail.com>
2013-02-11 21:52:36 +0100

    investigando cambios en la estructura

--- src/_h5ai/server/php/index.php
+++ src/_h5ai/server/php/index.php
@@ -2,8 +2,8 @@
 
 function normalize_path($path, $trailing_slash = false) {
 
-	$path = str_replace("\\", "/", $path);
-	return preg_match("#^(\w:)?/$#", $path) ? $path : (preg_replace('#/$#', '', $path) . ($trailing_slash ? "/" : ""));
+    $path = str_replace("\\", "/", $path);
+    return preg_match("#^(\w:)?/$#", $path) ? $path : (preg_replace('#/$#', '', $path) . ($trailing_slash ? "/" : ""));
 }
 
 define("APP_ABS_PATH", normalize_path(dirname(dirname(dirname(__FILE__)))));
@@ -13,7 +13,7 @@ define("ABS_HREF", normalize_path(preg_replace('/[^\\/]*$/', '', getenv("REQUEST
 
 function normalized_require_once($lib) {
 
-	require_once(APP_ABS_PATH . $lib);
+    require_once(APP_ABS_PATH . $lib);
 }
 
 normalized_require_once("/server/php/inc/util.php");
@@ -24,22 +24,22 @@ $app = new App(APP_ABS_PATH, APP_ABS_HREF, ABS_HREF);
 
 if (array_key_exists("action", $_REQUEST)) {
 
-	header("Content-type: application/json;charset=utf-8");
+    header("Content-type: application/json;charset=utf-8");
 
-	normalized_require_once("/server/php/inc/Api.php");
-	$api = new Api($app);
-	$api->apply();
+    normalized_require_once("/server/php/inc/Api.php");
+    $api = new Api($app);
+    $api->apply();
 
-	json_fail(100, "unsupported request");
+    json_fail(100, "unsupported request");
 
 } else {
 
-	header("Content-type: text/html;charset=utf-8");
+    header("Content-type: text/html;charset=utf-8");
 
-	$HREF = $app->get_app_abs_href();
-	$FALLBACK = $app->get_no_js_fallback();
+    $HREF = $app->get_app_abs_href();
+    $FALLBACK = $app->get_no_js_fallback();
 
-	normalized_require_once("/server/php/inc/page.php");
+    //normalized_require_once("/server/php/inc/page.php");
 }
 
 ?>
